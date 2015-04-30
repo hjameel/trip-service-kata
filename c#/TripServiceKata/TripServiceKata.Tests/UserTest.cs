b4822864392de641f77f2e468b0b498bb0c667c8
Hanif Jameel <hanif.jameel@btinternet.com>
2015-04-30 13:50:05 +0100

    Apply simple refactorings to the TripService.

---
+++ c#/TripServiceKata/TripServiceKata.Tests/UserTest.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+
+namespace TripServiceKata.Tests
+{/*
+    public class when_is_friends_with_is_called
+    {
+        [TestFixture]
+        public class given_users_are_not_friends
+        {
+            [Test]
+            public void it_should_return_false()
+            {
+                var user = new User.User();
+            }
+
+        }
+    }*/
+}
--- c#/TripServiceKata/TripServiceKata/Trip/TripService.cs
+++ c#/TripServiceKata/TripServiceKata/Trip/TripService.cs
@@ -8,29 +8,17 @@ namespace TripServiceKata.Trip
     {
         public List<Trip> GetTripsByUser(User.User user)
         {
-            List<Trip> tripList = new List<Trip>();
-            User.User loggedUser = GetLoggedInUser();
-            bool isFriend = false;
-            if (loggedUser != null)
-            {
-                foreach (User.User friend in user.GetFriends())
-                {
-                    if (friend.Equals(loggedUser))
-                    {
-                        isFriend = true;
-                        break;
-                    }
-                }
-                if (isFriend)
-                {
-                    tripList = FindTripsByUser(user);
-                }
-                return tripList;
-            }
-            else
+            if (GetLoggedInUser() == null)
             {
                 throw new UserNotLoggedInException();
             }
+            return UserIsFriendsWith(GetLoggedInUser(), user)
+                ? FindTripsByUser(user) : new List<Trip>();
+        }
+
+        bool UserIsFriendsWith(User.User loggedInUser, User.User otherUser)
+        {
+            return otherUser.GetFriends().Contains(loggedInUser);
         }
 
         protected virtual TripServiceKata.User.User GetLoggedInUser()
